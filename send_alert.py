25ff588891b6fc7655fca9d29cfbea88efc3d2a7
ysh0619 <98309588+ysh0619@users.noreply.github.com>
2025-05-15 20:52:31 +0900

    Update send_alert.py

--- send_alert.py
+++ send_alert.py
@@ -19,7 +19,7 @@ if hour < 8:
 
 # 메시지 전송
 next_hour = (hour + 1) % 24
-message = f"⏰ {next_hour:02}시 결계 5분 전입니다! @everyone"
+message = f"⏰ {next_hour:02}시 결계 3분 전입니다! @everyone"
 
 response = requests.post(webhook_url, json={"content": message})
 
