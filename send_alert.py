4dd6f3d277704e38c3bfdb3332d3aef79102a9ba
ysh0619 <98309588+ysh0619@users.noreply.github.com>
2025-05-15 17:08:57 +0900

    Update send_alert.py

--- send_alert.py
+++ send_alert.py
@@ -12,7 +12,7 @@ if not webhook_url:
 # 다음 정각 예측
 now = datetime.utcnow() + timedelta(hours=9)  # 한국 시간 기준 (UTC+9)
 next_hour = (now.hour + 1) % 24
-message = f"⏰ 결계 3분 전입니다! 준비하세요."
+message = f"⏰ 결계 3분 전입니다! 준비하세요. @everyone"
 
 response = requests.post(webhook_url, json={"content": message})
 
